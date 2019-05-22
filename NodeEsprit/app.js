c9c5aa37f56f9995d81474ce7e08c4adfb79f943
hphqlim <abdelhalimbenjmila@esprit.tn>
2019-05-22 22:15:36 +0100

    connection mysql et exemple webservice

--- NodeEsprit/app.js
+++ NodeEsprit/app.js
@@ -6,22 +6,36 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+var mysql = require("mysql");
+
+var service = require("./routes/service");
 
 // create express app
 const app = express();
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({extended: true}))
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
-
+app.use(function (req, res, next) {
+    res.locals.connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'fixit'
+    });
+    res.locals.connection.connect();
+    next();
+});
 // define a simple route
+app.use("/service", service);
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    res.json({"message": "Welcome to nodesprit application"});
 });
 
 // listen for requests
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
+module.exports = app;
